2321c703d15101cd3c4e8633eaece604ab5c8356
Sean Stasiak <1123801+essess@users.noreply.github.com>
2015-07-31 12:58:40 -0400

    initial commit

---
+++ src/main.c
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2015 Sean Stasiak. All rights reserved.
+ * Developed by: Sean Stasiak <sstasiak@gmail.com>
+ * Refer to license terms in license.txt; In the absence of such a file,
+ * contact me at the above email address and I can provide you with one.
+ */
+
+void main( void )
+{
+}
